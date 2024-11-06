95d3522f4c9c177835fa9662838f4da388bd7743
mdavi89 <83469622+mdavi89@users.noreply.github.com>
2024-11-06 09:11:30 -0500

    Added nav function to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Nav from './components/Navigation.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Nav >
+      <App />
+    </Nav>
   </StrictMode>,
 )

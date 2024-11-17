9bb2f432533b0e7b2bea30f9a072c74ce57d93e6
Matthew Davis <mdavi359@yahoo.com>
2024-11-17 18:59:36 -0500

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,31 @@
+import React from "react";
+
 function Footer() {
-
-};
-
-export default Footer;
+    return (
+      <footer className="footer">
+        <a href="/about">
+          <img
+            src="/github-mark.png"
+            alt="GitHub"
+            className="footer-gif"
+          />
+        </a>
+        <a href="/contact">
+          <img
+            src="/In-Blue-72.png"
+            alt="LinkedIn"
+            className="footer-gif"
+          />
+        </a>
+        <a href="/privacy-policy">
+          <img
+            src="/stack-logo.png"
+            alt="Stack Overflow"
+            className="footer-gif"
+          />
+        </a>
+      </footer>
+    );
+  }
+  
+  export default Footer;
